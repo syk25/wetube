6bd3a427ac57e4aaffac53808db7609260b6ea84
syk25 <syk2245@gmail.com>
2024-09-14 16:22:54 +0900

    #3.1 ~ #3.3 Requests and Responses

--- src/server.js
+++ src/server.js
@@ -4,13 +4,26 @@ const PORT = 4000;
 
 const app = express(); // Express application 생성
 
-/* NOTE: 서버란?
-서버는 항상 켜져 있고 인터넷에 연결되어 있는 컴퓨터다.
-서버는 request를 기다리고 있다.
-request란 연결요쳥이다.
- */
+/* NOTE: GET 요청 */
+const handleHome = (req, res) => {
+    // req: 서버한테 전달되는 내용을 표현하는 객체
+    // console.log(req);
+    // console.log(res);
 
+    // return res.end(); // 요청을 끝내는 법 중에 하나 - 파이썬의 pass와 비슷한 역할 - 프로그램의 무한 실행을 중단시킴
+    return res.send("I still love you.");
+}; // NOTE: route handler
+
+app.get("/", handleHome); // 요청은 받았지만 아직 응답은 아직 하지는 않았다.
+
+/* NOTE: 서버 연결에 대한 콜백함수 */
 const handleListening = () =>
     console.log(`Server is listening on port http://localhost:${PORT} 🚀`);
 
+/* NOTE:서버에 연결 */
 app.listen(PORT, handleListening); // localhost:4000 에 접속 가능
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
+app.get("/login", handleLogin);

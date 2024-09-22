d35775f24bc4d4986dabf5afedd5248e349c8aa9
syk25 <syk2245@gmail.com>
2024-09-22 14:40:00 +0900

    # 3.6 middlewares part2

--- src/server.js
+++ src/server.js
@@ -8,11 +8,20 @@ const handleListening = () =>
 const app = express();
 app.listen(PORT, handleListening);
 
-const gossipMiddleware = (req, res, next) => {
-    // 미들웨어는 요청에 응답하지 않고 다른 함수에게 요청을 토스함
-    // next 함수 실행시 미들웨어라는 의미
-    console.log(`Someone is going to request: ${req.url}`);
-    next(); // 다음에 연결 된 함수 실행
+const logger = (req, res, next) => {
+    console.log(`The action requested: ${req.method} ${req.url}`);
+    next();
+};
+
+/* NOTE: 미들웨어를 통해 로직을 추가할 수 있다. + url 검증도 가능 */
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not allowed</h1>");
+    } else {
+        console.log("The url is allowed. You may continue");
+        next();
+    }
 };
 
 const handleHome = (req, res) => {
@@ -23,5 +32,16 @@ const handleLogin = (req, res) => {
     return res.send({ message: "Login here." });
 };
 
-app.get("/", gossipMiddleware, handleHome); // 미들웨어 실행 후 다음 함수 실행, 여기서 handleHome은 finalware - get 메서드 설명을 보면 헨들러는 여러개 등록 가능
+/* NOTE:  */
+const handleProtected = (req, res) => {
+    console.log("Welcome to the private lounge");
+};
+
+/* NOTE: use-get 순서 보장하기! */
+app.use(logger); // 해당 함수는 아래서부터의 경로에서 호출 된다
+app.use(privateMiddleware);
+
+app.get("/", handleHome); // 미들웨어 실행 후 다음 함수 실행, 여기서 handleHome은 finalware - get 메서드 설명을 보면 헨들러는 여러개 등록 가능
+app.get("/private", handleProtected);
+
 app.get("/login", handleLogin);

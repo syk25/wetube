aac3b7dee4643196224564c9ba23b403da88232a
syk25 <syk2245@gmail.com>
2024-10-21 15:31:18 +0900

    refresh

--- src/controllers/userControllers.js
+++
@@ -1,6 +0,0 @@
-export const join = (req, res) => res.send("Join");
-export const edit = (req, res) => res.send("Edit user");
-export const remove = (req, res) => res.send("Remove user");
-export const login = (req, res) => res.send("Login");
-export const see = (req, res) => res.send("See User");
-export const logout = (req, res) => res.send("Logout");

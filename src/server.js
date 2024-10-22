34a39548030d8d244c97028aab360e31bbe5f452
syk25 <syk2245@gmail.com>
2024-10-22 15:50:49 +0900

    feat: add home endpoint

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import express from "express";
 
 const app = express();
 const PORT = 4000;
-app.listen(PORT, () =>
-    console.log(
-        `Connected to http://localhost:${PORT} 🚀`
-    )
-);
+app.listen(PORT, () => console.log(`Connected to http://localhost:${PORT} 🚀`));
+
+const handleHome = (req, res) => {
+    console.log("current route: http://localhost");
+    return res.end();
+};
+
+app.get("/", handleHome);

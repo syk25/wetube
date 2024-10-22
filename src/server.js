7ddd6bfe0633db68811fa7552ae95d2924e8d1a2
syk25 <syk2245@gmail.com>
2024-10-22 15:13:55 +0900

    feat: implment express server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("Hello NodeJS!!!");
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+app.listen(PORT, () =>
+    console.log(
+        `Connected to http://localhost:${PORT} 🚀`
+    )
+);

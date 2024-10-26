2e807858578769cc04ca18ae1d67af36c519ec4a
syk25 <syk2245@gmail.com>
2024-10-27 03:33:07 +0900

    view 생성

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("Home page videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search videos");
-export const see = (req, res) => res.send("See videos");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("Delete video");
 export const upload = (req, res) => res.send("Upload video");

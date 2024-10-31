2984835cfba4360054c505ed7370e0e8e9d3d46c
syk25 <syk2245@gmail.com>
2024-10-31 19:49:22 +0900

    templates: pug - mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,19 @@
+const Video = (myTitle, myRating, myComments, myCreatedAt, myViews, myId) => {
+    return {
+        title: myTitle,
+        rating: myRating,
+        comments: myComments,
+        createdAt: myCreatedAt,
+        views: myViews,
+        id: myId,
+    };
+};
+
 const videos = [
-    {
-        title: "Video1",
-    },
-    {
-        title: "Video2",
-    },
-    {
-        title: "Video3",
-    },
+    Video("First Video", 5, 296, "2 minutes ago", 590, 1),
+    Video("Second Video", 2, 10, "2 minutes ago", 691, 2),
+    Video("Third Video", 4, 200, "2 minutes ago", 400, 3),
+    Video("Fourth Video", 3, 21, "2 minutes ago", 629, 4),
 ];
 export const trending = (req, res) =>
     res.render("home", { pageTitle: "Home", videos });

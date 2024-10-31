684ae2ff24fc44ff081db85962255d8d58c4db43
syk25 <syk2245@gmail.com>
2024-10-31 18:05:34 +0900

    templates: pug - iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,16 @@
-const fakeUser = {
-    name: "Ben",
-    loggedIn: true,
-};
+const videos = [
+    {
+        title: "Video1",
+    },
+    {
+        title: "Video2",
+    },
+    {
+        title: "Video3",
+    },
+];
 export const trending = (req, res) =>
-    res.render("home", { pageTitle: "Home", fakeUser });
+    res.render("home", { pageTitle: "Home", videos });
 export const search = (req, res) => res.send("Search videos");
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });

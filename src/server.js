746587c6c5f75a8c4a595672937b541f04b103e6
syk25 <syk2245@gmail.com>
2024-11-07 07:14:06 +0900

    세션과 쿠키

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,6 +12,26 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+// 라우트 이전에 세션 설정
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id} ${req.session.potato}`);
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);

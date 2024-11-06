ba61d4b445899cbeefc4d9ee3116aaa0a3fe545a
syk25 <syk2245@gmail.com>
2024-11-07 06:14:04 +0900

    postLogin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postJoin = async (req, res) => {
     const { name, username, email, password, password2, location } = req.body;
     const pageTitle = "Join";
@@ -17,21 +19,55 @@ export const postJoin = async (req, res) => {
             errorMessage: "This username/email is already taken.",
         });
     }
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    });
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle: "Upload Video",
+            errorMessage: "This username/email is already taken.",
+        });
+    }
 };
 
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => res.send("Login");
+
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+
+    const ok = await bcrypt.compare(password, user.password);
+
+    if (!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password",
+        });
+    }
+
+    res.redirect("/");
+};
+
 export const logout = (req, res) => res.send("Logout");
+
 export const see = (req, res) => {
     console.log(req.params);
     res.send("See profile");
 };
+
 export const edit = (req, res) => res.send("Edit User");
+
 export const deleteUser = (req, res) => res.send("Remove User");

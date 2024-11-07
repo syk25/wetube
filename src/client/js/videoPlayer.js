4802f970f7a91bfa0c818f0132a68d0e98f353bb
syk25 <syk2245@gmail.com>
2024-11-08 04:31:21 +0900

    play pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,28 @@
-console.log("Video player");
+const video = document.querySelector("video");
+
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+const handlePlay = () => {
+    playBtn.innerText = "Pause";
+};
+const handlePause = () => {
+    playBtn.innerText = "Play";
+};
+
+const handleMute = (e) => {};
+
+play.addEventListener("click", handlePlayClick);
+mute.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);

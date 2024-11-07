90a2ef46622ce11d803c698f35905636b8a82f0f
syk25 <syk2245@gmail.com>
2024-11-07 17:58:10 +0900

    post edit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -72,8 +72,25 @@ export const getEdit = (req, res) => {
     });
 };
 
-export const postEdit = (req, res) => {
-    return res.render("edit-profile");
+export const postEdit = async (req, res) => {
+    const {
+        session: {
+            user: { _id },
+        },
+        body: { name, email, username, location },
+    } = req;
+    const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        {
+            name,
+            email,
+            username,
+            location,
+        },
+        { new: true }
+    );
+    req.session.user = updatedUser;
+    return res.redirect("/users/edit");
 };
 
 export const deleteUser = (req, res) => res.send("Remove User");

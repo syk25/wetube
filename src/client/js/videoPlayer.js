7b9d3e731b376beb2e526131347884768328686d
syk25 <syk2245@gmail.com>
2024-11-08 07:25:12 +0900

    volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,9 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
     if (video.paused) {
@@ -11,18 +13,30 @@ const handlePlayClick = (e) => {
     } else {
         video.pause();
     }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePlay = () => {
-    playBtn.innerText = "Pause";
+const handleMuteClick = (e) => {
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
 };
-const handlePause = () => {
-    playBtn.innerText = "Play";
+const handleVolumeChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
 };
 
-const handleMute = (e) => {};
-
-play.addEventListener("click", handlePlayClick);
-mute.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
